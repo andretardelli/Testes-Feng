c0c9ab90db819b45384cb082359576da46fa9aec
André <andretardelli@gmail.com>
2018-05-31 02:33:22 -0300

    Comentários gerais no código

--- Front end/scripts/scripts.js
+++ Front end/scripts/scripts.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-    $('.selectpicker').selectpicker({
-        style: 'btn-default',
-        size: false,
-    });
 
+	// Inserindo máscaras nos campos de CPF e cartão
      $('#InputCPF').mask('999.999.999-99');
      $('#InputCPF1').mask('999.999.999-99');
      $('#Cartao').mask('9999 9999 9999 9999');
