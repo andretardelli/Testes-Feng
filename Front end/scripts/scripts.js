f8faeb2ce68002ac69d59a1e9715f1257b5afe8a
André <andretardelli@gmail.com>
2018-05-31 01:23:05 -0300

    Ajuste de campos e máscaras

--- Front end/scripts/scripts.js
+++ Front end/scripts/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
         style: 'btn-default',
         size: false,
     });
+
+     $('#InputCPF').mask('999.999.999-99');
+     $('#InputCPF1').mask('999.999.999-99');
+     $('#Cartao').mask('9999 9999 9999 9999');
 });
